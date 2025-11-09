79ee3541c12c83ca1a1fd148d4902e4939b40158
Ahmad Naufal <ahmd.naufal296@gmail.com>
2025-11-09 21:38:02 +0700

    feat(main): implement migration with gorm

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"pustaka-api/books"
+	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
-	"pustaka-api/handler"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "host=100.105.102.88 user=developer password=password dbname=pustaka_api port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Db connection error")
+	}
+
+	fmt.Println("Database connection succeed")
+
+	//migration
+	db.AutoMigrate(books.Book{})
+	
+	//CRUD
+	book := books.Book{}
+	book.Title = "Manusia Kuat"
+	book.Price = 90000
+	book.Rating = 5
+	book.Description = "Lorem ipsum"
+
+	err = db.Create(&book).Error
+
+	if err != nil {
+		fmt.Print("Error Creating book record")
+	}
+
 	router := gin.Default()
 
 	// Versioning routes with group
 	v1 := router.Group("/v1")
-	
+
 	// GET ROUTE
 	v1.GET("/", handler.RootHandler)
 
@@ -26,4 +56,3 @@ func main() {
 
 	router.Run(":3000")
 }
-
