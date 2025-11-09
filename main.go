e5d37fdbdf6a72ba6209f82442714593d7a9266b
Ahmad Naufal <ahmd.naufal296@gmail.com>
2025-11-09 16:51:13 +0700

    feat(main): implement post endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,13 +10,17 @@ import (
 func main() {
 	router := gin.Default()
 
+	// GET ROUTE
 	router.GET("/", rootHandler)
 
 	router.GET("/book/:bookId", booksHandler)
-	
+
 	router.GET("/book/:bookId/author/:authorId", authorsHandler)
 
 	router.GET("/query", queryHandler)
+
+	// POST ROUTE
+	router.POST("/book", postBookHandler)
 
 	router.Run(":3000")
 }
@@ -34,11 +39,11 @@ func booksHandler(ctx *gin.Context) {
 
 func authorsHandler(ctx *gin.Context) {
 	bookId := ctx.Param("bookId")
-	authorId  := ctx.Param("authorId")
+	authorId := ctx.Param("authorId")
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"authorId": authorId,
-		"bookId": bookId,
+		"bookId":   bookId,
 	})
 }
 
@@ -46,4 +51,25 @@ func queryHandler(ctx *gin.Context) {
 	title := ctx.Query("title")
 	author := ctx.Query("author")
 	ctx.JSON(http.StatusOK, gin.H{"title": title, "author": author})
+}
+
+type bookInput struct {
+	Title string
+	Price int
+	SubTitle  string `json:"sub_title"` // alias as sub_title
+}
+
+func postBookHandler(ctx *gin.Context) {
+	var bookInput bookInput
+
+	err := ctx.ShouldBindBodyWithJSON(&bookInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+		"sub_title": bookInput.SubTitle,
+	})
 }
